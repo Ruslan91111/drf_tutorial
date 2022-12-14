fd373362e722591ddfdda7ce8fee0a54d607b832
Ruslan9111 <ruslan_ru911@mail.ru>
2022-12-15 04:46:02 +0500

    simplest view without serializer

--- drfmovies/movies/views.py
+++ drfmovies/movies/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
@@ -7,10 +8,20 @@ from .models import Movies
 from .serializers import MoviesSerializer
 
 
-
 class MoviesAPIView(APIView):
     def get(self, request):
-        return Response({'title': 'terminator'})
+        list_of_movies = Movies.objects.all().values()
+        return Response({'movies': list(list_of_movies)})
+
+    def post(self, request):
+        movie_new = Movies.objects.create(
+            title=request.data['title'],
+            plot=request.data['plot'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'movie': model_to_dict(movie_new)})
+
 
 
 
